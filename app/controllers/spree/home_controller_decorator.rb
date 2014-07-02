1a0ae21f4a7b9322baff6cf9bf4502c1d36c0c64
tailic <niklas@derschreiber.com>
2014-07-02 12:52:41 +0200

    add product scopes by taxon for featured, slider and latest

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -1,3 +1,17 @@
 Spree::HomeController.class_eval do
   layout 'home_rowsets'
+
+  def index
+    @searcher = build_searcher(params)
+    @products = @searcher.retrieve_products
+
+    slider = Spree::Taxon.where(:name => 'Slider').first
+    @slider_products = slider.products.active.distinct if slider
+
+    featured = Spree::Taxon.where(:name => 'Featured').first
+    @featured_products = featured.products if featured
+
+    latest = Spree::Taxon.where(:name => 'Latest').first
+    @latest_products = latest.products.active.distinct if latest
+  end
 end
